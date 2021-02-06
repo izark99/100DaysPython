cf82b4342a6747778164b4dba0869188a1b83346
izark <izarknguyen@gmail.com>
2021-02-06 14:28:52 +0700

    Day 3.2 Extra

--- day3.2_rollercoaster.py
+++ day3.2_rollercoaster.py
@@ -1,13 +1,21 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 if height >= 120:
+    ticketPrice = 0
     print("You can ride the rollercoaster!")
     age = int(input("How old are you? "))
     if age < 12:
-        print("Please pay $5.")
+        print("Child ticket are $5.")
+        ticketPrice += 5
     elif age <= 18:
-        print("Please pay $7.")
+        print("Youth ticket are $7.")
+        ticketPrice += 7
     else:
-        print("Please pay $12.")
+        print("Adult ticket are $12.")
+        ticketPrice += 12
+    takeAPhoto = input("Do you want to take a photo while ride rollercoaster? Y or N ")
+    if takeAPhoto.upper() == "Y":
+        ticketPrice += 3
+    print(f"Your ticket price is ${ticketPrice}")
 else:
     print("Sorry! you have to grow taller before you can ride")
