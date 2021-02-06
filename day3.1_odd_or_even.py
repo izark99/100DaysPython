87dcf9ed9d30734ac28843e97b7710e040da9e3e
izark <izarknguyen@gmail.com>
2021-02-06 13:41:58 +0700

    Day 3.1

---
+++ day3.1_odd_or_even.py
@@ -0,0 +1,6 @@
+print("Odd or even tool")
+number = int(input("Enter your number: "))
+if number % 2 == 0:
+    print(f"Your number {number} is even")
+else:
+    print(f"Your number {number} is odd")
