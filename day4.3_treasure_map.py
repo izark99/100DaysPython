b7e75d2cddc90fa578241552bcef06ce5b932ad0
izark <izarknguyen@gmail.com>
2021-02-12 23:29:28 +0700

    Day 4.3

---
+++ day4.3_treasure_map.py
@@ -0,0 +1,11 @@
+row1= ['📦', '📦', '📦']
+row2= ['📦', '📦', '📦']
+row3= ['📦', '📦', '📦']
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}\n")
+position = input("Where do you want to put treasure? ")
+
+col = int(position[0])
+row = int(position[1])
+map[row-1][col-1] = "💰"
+print(f"{row1}\n{row2}\n{row3}\n")
