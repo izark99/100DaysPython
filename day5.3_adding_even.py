9980d0992340b42a4f75a742f262be083feb690e
izark <izarknguyen@gmail.com>
2021-02-13 00:20:22 +0700

    Day 5.3

---
+++ day5.3_adding_even.py
@@ -0,0 +1,6 @@
+print("Welcome to calculator even number from 0 to your choose number!")
+end_number = int(input("Enter your number: "))
+sum = 0
+for i in range(0, end_number, 2):
+        sum += i
+print(f"Result: {sum}")
